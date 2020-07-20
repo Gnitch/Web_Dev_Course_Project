44e118f46f690761b9c73d5bfca6a885cfea0ec6
Gnitch <sujoydcunha@gmail.com>
2020-07-20 17:40:52 +0530

    Add model

--- quiz/models.py
+++ quiz/models.py
@@ -1,2 +1,28 @@
 from django.db import models
 
+class Quiz(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    answer = models.TextField(blank=False,null=False)
+
+class Options(models.Model):
+    options = models.TextField(blank=True,null=True)
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+
+
+
+
+
+
+
+
+
+
+
+
